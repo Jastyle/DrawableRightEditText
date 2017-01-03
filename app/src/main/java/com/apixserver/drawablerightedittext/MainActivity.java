b9aac17b71036cafaeb0e1d3f31936a96df95b82
Jastyle <woshigebai@gmail.com>
2017-01-03 14:48:51 +0800

    add click other region clearfocus

--- app/src/main/java/com/apixserver/drawablerightedittext/MainActivity.java
+++ app/src/main/java/com/apixserver/drawablerightedittext/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawableRightEditText rightEditText_cancel, rightEditText_pswd;
-    private static boolean pswdstatus = false;
+    private DrawableRightEditText rightEditText_cancel, rightEditText_pswd,editable_et;
+    private static boolean editstatus = true;//是否可编辑
+    private static boolean pswdstatus = false;//是否显示密码
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private void findView() {
         rightEditText_cancel = (DrawableRightEditText) findViewById(R.id.edit_cancel);
         rightEditText_pswd = (DrawableRightEditText) findViewById(R.id.edit_pswd);
+        editable_et = (DrawableRightEditText) findViewById(R.id.editable_et);
         rightEditText_cancel.setTextChangeListener(new TextChangeListener() {
             @Override
             public void afterTextChanged(Editable editable) {
@@ -75,6 +78,78 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        editable_et.setTextChangeListener(new TextChangeListener() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onClickDrawRight(EditText editText) {
+                if (editstatus) {//可编辑
+                    editable_et.setFocusableInTouchMode(true);
+                    editable_et.setSelection(editable_et.getText().toString().trim().length());
+                    editable_et.setDrawable(getResources().getDrawable(R.mipmap.icon_cancel));
+                    editstatus = false;
+                }else {
+                    editable_et.setText("");
+                }
+            }
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    editable_et.setFocusableInTouchMode(false);
+                    editable_et.setDrawable(getResources().getDrawable(R.mipmap.icon_edit));
+                    editable_et.setRightDrawableVisible(true);
+                    editstatus = true;
+                }else {
+                    editable_et.setFocusableInTouchMode(true);
+                    editable_et.setDrawable(getResources().getDrawable(R.mipmap.icon_cancel));
+                    editable_et.setRightDrawableVisible(true);
+                    editstatus = false;
+                }
+            }
+        });
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            isShouldHideInput(v,ev);
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
     }
 
+    public  boolean isShouldHideInput(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = { 0, 0 };
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                //清除焦点
+                v.clearFocus();
+                return true;
+            }
+        }
+        return false;
+    }
 }
